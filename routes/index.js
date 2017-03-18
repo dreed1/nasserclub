fed29dc032a8a8917dba080f80a758f93e2848e9
Dan Reed <danreedx86@gmail.com>
2017-03-18 12:25:39 -0700

    replace jade with pug, import picture of nasser

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nasser Mohamed Fan Club' });
+  res.render('index', { title: 'Nasser Mohamed Fan Club', nasserImage: 'images/nasser_is_the_sun.jpg'});
 });
 
 module.exports = router;
